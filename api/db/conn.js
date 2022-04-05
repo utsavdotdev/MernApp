134e911956039a82ab4d7c492d033709d292d6cf
Rajesh Khadka <rajeshkhadkaofficial45@gmail.com>
2022-04-05 23:33:02 +0545

    added

--- api/db/conn.js
+++ api/db/conn.js
@@ -1,13 +1,17 @@
 const mongoose = require("mongoose");
 
 mongoose
-  .connect("mongodb://localhost:27017/empapi", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(
+    "mongodb+srv://utsavbhattarai:utsav+123@cluster0.rjrax.mongodb.net/mernapp?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
   .then(() => {
     console.log("Connection Successful");
   })
   .catch((err) => {
     console.log(err);
+    console.log("err");
   });
