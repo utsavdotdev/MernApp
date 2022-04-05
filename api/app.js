e6596c671a2d714923ef5e4f89ed42a00f005e86
Rajesh Khadka <rajeshkhadkaofficial45@gmail.com>
2022-04-05 23:35:07 +0545

    added backend db

--- api/app.js
+++ api/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const empModel = require("./models/employee");
 const empRouter = require("./routers/emp");
 const app = express();
 const port = process.env.PORT || 3001;
@@ -10,8 +11,13 @@ require("./db/conn");
 app.use(express.json({ extented: false }));
 app.use(cors());
 app.use(empRouter);
-app.get("/", (req, res) => {
-  res.send("hello");
+app.get("/", async (req, res) => {
+  try {
+    const emp = await empModel.find();
+    res.send(emp);
+  } catch (error) {
+    res.send(error);
+  }
 });
 app.listen(port, () => {
   console.log(`Listening on ${port}`);
